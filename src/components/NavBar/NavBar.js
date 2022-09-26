6722774cd7bea6ca4e2efc093f39812aa95a4969
Brandon <brandon.urtecho@unmsm.edu.pe>
2022-09-25 22:25:03 -0500

    PreEntrega1Urtecho

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,16 +5,18 @@ import CartWidget from '../CartWidget/CartWidget';
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__categories">
-        <span className="category">Hombre</span>
-        <span className="category">Mujer</span>
-        <span className="category">Infantil</span>
-      </div>
       <div className="navbar__logo">
         <h1>MarkShop</h1>
       </div>
-      <div className="navbar__menu">
-        <CartWidget></CartWidget>
+      <div className="navbar__right">
+        <div className="navbar__categories">
+          <span className="category">Hombre</span>
+          <span className="category">Mujer</span>
+          <span className="category">Infantil</span>
+        </div>
+        <div className="navbar__menu">
+          <CartWidget></CartWidget>
+        </div>
       </div>
     </nav>
   );
